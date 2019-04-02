Happyektoo/pytorch

commit df90eb75b25c4198b7360cbb8c3bc8f50c4ec1ba

Some fixes for the build script on Windows (#18681)
Summary:
Fixes https://discuss.pytorch.org/t/pytorch-build-from-source-on-windows/40288/13?u=peterjc123.
Pull Request resolved: https://github.com/pytorch/pytorch/pull/18681

Differential Revision: D14711039

Pulled By: soumith

fbshipit-source-id: f7e1a94b163064c055670b2925cd4502e7773599

tools/build_pytorch_libs.py
@@ -112,6 +112,13 @@ def create_build_env():
 
     if IS_WINDOWS:
         my_env = overlay_windows_vcvars(my_env)
+        # When using Ninja under Windows, the gcc toolchain will be chosen as default.
+        # But it should be set to MSVC as the user's first choice.
+        if USE_NINJA:
+            cc = my_env.get('CC', 'cl')
+            cxx = my_env.get('CXX', 'cl')
+            my_env['CC'] = cc
+            my_env['CXX'] = cxx
     return my_env
 
 
@@ -258,7 +265,7 @@ def build_caffe2(version,
             check_call(['cmake', '--build', '.', '--target', 'install', '--config', build_type, '--', '-j', str(j)],
                        cwd=build_dir, env=my_env)
         else:
-            check_call(['msbuild', 'INSTALL.vcxproj', '/p:Configuration={}'.format(build_type)],
+            check_call(['msbuild', 'INSTALL.vcxproj', '/p:Configuration={} /maxcpucount:{}'.format(build_type, j)],
                        cwd=build_dir, env=my_env)
     else:
         if USE_NINJA:

commit 1a3acf85a1bc999cf4dd46a1a91645d0d56c40a5

Fix windows msbuild bug (#18748)
Summary:
Fix the bug introduced by #18681 where an undefined variable was being used to limit max cpu count when building for Windows without Ninja.
Pull Request resolved: https://github.com/pytorch/pytorch/pull/18748

Differential Revision: D14733209

Pulled By: soumith

fbshipit-source-id: 52fc0dd4dde99da75a6956b63f02da2e647eed4f

tools/build_pytorch_libs.py
@@ -265,6 +265,7 @@ def build_caffe2(version,
             check_call(['cmake', '--build', '.', '--target', 'install', '--config', build_type, '--', '-j', str(j)],
                        cwd=build_dir, env=my_env)
         else:
+            j = max_jobs or str(multiprocessing.cpu_count())
             check_call(['msbuild', 'INSTALL.vcxproj', '/p:Configuration={} /maxcpucount:{}'.format(build_type, j)],
                        cwd=build_dir, env=my_env)
     else: