Happyektoo/pytorch

commit 12fbcd3ccdcac70903090a39d1100697783681d2

Refactor CUDA copy and general copy dispatch (#20685)
Summary:
Copy.cu goes from 308 to 190 lines of code. In general it uses, the same
copy strategy, using cudaMempcyAsync, a pointwise kernel, or a copy
using temporary buffers. The pointwise kernel has slightly improved
performance when broadcasting due to faster index calculation.

This deletes "`s_copy_`", "`_s_copy_from`", and "`_copy_same_type_`". The only
entry-point now is "`copy_`".

A mini-benchmark is here:
https://gist.github.com/colesbury/706de1d4e8260afe046020988410b992

Before:
https://gist.github.com/colesbury/ab454b6fe3791bff420d7bcf8c041f18
After:
https://gist.github.com/colesbury/9024d242b56ab09a9ec985fa6d1620bc

Results were measured on 2.2 GHz Broadwell; no-turbo; one thread;
compiled with GCC 7.3.0. (Results are slower than typical usage due to
turbo being off.)

The only significant differences is in the CUDA [1024] -> [1024, 1024]
broadcasting copy which is ~25% faster. I don't expect a noticeable
difference in real programs.

CPU copy overhead is a tiny bit (~200 ns) faster, but I don't expect
anyone to notice that.
Pull Request resolved: https://github.com/pytorch/pytorch/pull/20685

Differential Revision: D15414819

Pulled By: colesbury

fbshipit-source-id: d3c6e04a5020470e3bef15b1fc09503cae5df440

aten/src/ATen/native/Copy.cpp
@@ -1,57 +1,25 @@
 #include <ATen/native/Copy.h>
 
 #include <ATen/ATen.h>
-#include <ATen/CPUApplyUtils.h>
 #include <ATen/Dispatch.h>
-#include <ATen/ExpandUtils.h>
 #include <ATen/NativeFunctions.h>
-#include <ATen/native/cpu/CopyKernel.h>
+#include <ATen/native/TensorIterator.h>
+#include <ATen/native/quantized/Copy.h>
 
 namespace {
 
-bool copy_transpose_valid(const at::Tensor& self, const at::Tensor& src) {
+using namespace at;
+
+bool copy_transpose_valid(const Tensor& self, const Tensor& src) {
   const int MIN_SZ = 60 * 60;
   return self.is_contiguous() && src.numel() != 0 && src.dim() == 2 &&
       src.stride(0) == 1 && src.stride(1) == src.size(0) &&
       self.numel() >= MIN_SZ;
 }
 
-} // namespace
-
-namespace at {
-namespace native {
-
-Tensor & copy_(Tensor & self, const Tensor & src, bool non_blocking) {
-  Tensor b_src;
-  if (self.is_sparse() && src.is_sparse()) {
-    return at::copy_sparse_to_sparse_(self, src, non_blocking);
-  }
-  if (!self.is_sparse() && !src.is_sparse()) {
-    std::tie(b_src) = expand_inplace(self, src, "copy");
-    return s_copy_(self, b_src, non_blocking);
-  }
-  AT_ERROR("copy_() between dense and sparse Tensors is not implemented! Found self type = ",
-           self.type(), " and src type = ", src.type());
-}
-
-Tensor& _s_copy__cpu(Tensor& self, const Tensor& src, bool non_blocking) {
-  if (src.type_id() != CPUTensorId()) {
-    _s_copy_from(src, self, non_blocking);
-    return self;
-  }
-
-  if (self.scalar_type() == src.scalar_type()) {
-    copy_kernel_same_type(kCPU, self, src);
-  } else {
-    TORCH_CHECK(self.numel() == src.numel(), "sizes do not match");
-    copy_kernel_cast(kCPU, self, src);
-  }
-  return self;
-}
-
 // special case copy where tensor is contiguous and src is a transposed matrix
-// This can be generalized to most copies, but it's tricker
-void _copy_same_type_transpose_(Tensor& self, const Tensor& src) {
+// This can be generalized to most copies, but it's trickier
+void copy_same_type_transpose_(Tensor& self, const Tensor& src) {
   int64_t BLOCK_SZ;
   if (self.scalar_type() == kByte) {
     BLOCK_SZ = 120;
@@ -60,62 +28,99 @@ void _copy_same_type_transpose_(Tensor& self, const Tensor& src) {
   }
   Tensor buf = empty({BLOCK_SZ, BLOCK_SZ}, self.options());
 
-  AT_DISPATCH_ALL_TYPES_AND(
-    at::ScalarType::Half, self.scalar_type(), "_copy_same_type_transpose_", [&]() {
-        scalar_t* sp = src.data<scalar_t>();
-        scalar_t* rp = self.data<scalar_t>();
-        scalar_t* bp = buf.data<scalar_t>();
+  AT_DISPATCH_ALL_TYPES_AND2(kHalf, kBool, self.scalar_type(), "copy_", [&] {
+    scalar_t* sp = src.data<scalar_t>();
+    scalar_t* rp = self.data<scalar_t>();
+    scalar_t* bp = buf.data<scalar_t>();
 
-        int64_t NR = src.size(0);
-        int64_t NC = src.size(1);
-        for (int64_t R = 0; R < NR; R += BLOCK_SZ) {
-          for (int64_t C = 0; C < NC; C += BLOCK_SZ) {
-            scalar_t* spo = sp + R + C * NR;
-            scalar_t* rpo = rp + C + R * NC;
+    int64_t NR = src.size(0);
+    int64_t NC = src.size(1);
+    for (int64_t R = 0; R < NR; R += BLOCK_SZ) {
+      for (int64_t C = 0; C < NC; C += BLOCK_SZ) {
+        scalar_t* spo = sp + R + C * NR;
+        scalar_t* rpo = rp + C + R * NC;
 
-            int nr = std::min(NR - R, BLOCK_SZ);
-            int nc = std::min(NC - C, BLOCK_SZ);
+        int nr = std::min(NR - R, BLOCK_SZ);
+        int nc = std::min(NC - C, BLOCK_SZ);
 
-            // 1. copy columns from src to buf
-            for (int c = 0; c < nc; c++) {
-              memcpy(bp + c * BLOCK_SZ, spo + c * NR, nr * sizeof(scalar_t));
-            }
+        // 1. copy columns from src to buf
+        for (int c = 0; c < nc; c++) {
+          memcpy(bp + c * BLOCK_SZ, spo + c * NR, nr * sizeof(scalar_t));
+        }
 
-            // 2. transpose buf in place
-            int rc_max = std::max(nr, nc);
-            int rc_min = std::min(nr, nc);
-            for (int r = 0; r < rc_max; r++) {
-              int end = std::min(r, rc_min);
-              for (int c = 0; c < end; c++) {
-                scalar_t tmp = bp[r + BLOCK_SZ * c];
-                bp[r + BLOCK_SZ * c] = bp[r * BLOCK_SZ + c];
-                bp[r * BLOCK_SZ + c] = tmp;
-              }
-            }
-
-            // 3. copy rows from buf to dst
-            for (int r = 0; r < nr; r++) {
-              memcpy(rpo + r * NC, bp + r * BLOCK_SZ, nc * sizeof(scalar_t));
-            }
+        // 2. transpose buf in place
+        int rc_max = std::max(nr, nc);
+        int rc_min = std::min(nr, nc);
+        for (int r = 0; r < rc_max; r++) {
+          int end = std::min(r, rc_min);
+          for (int c = 0; c < end; c++) {
+            scalar_t tmp = bp[r + BLOCK_SZ * c];
+            bp[r + BLOCK_SZ * c] = bp[r * BLOCK_SZ + c];
+            bp[r * BLOCK_SZ + c] = tmp;
           }
         }
-      });
+
+        // 3. copy rows from buf to dst
+        for (int r = 0; r < nr; r++) {
+          memcpy(rpo + r * NC, bp + r * BLOCK_SZ, nc * sizeof(scalar_t));
+        }
+      }
+    }
+  });
 }
 
-void _copy_same_type__cpu(Tensor& self, const Tensor& src) {
+} // namespace
+
+namespace at {
+namespace native {
+
+Tensor & copy_(Tensor & self, const Tensor & src, bool non_blocking) {
+  // TODO: this should be handled during dispatch, but that's missing...
+  TORCH_CHECK(self.defined(), "self is undefined");
+  TORCH_CHECK(self.defined(), "src is undefined");
+
+  Tensor b_src;
+  if (self.is_sparse() && src.is_sparse()) {
+    return at::copy_sparse_to_sparse_(self, src, non_blocking);
+  } else if (self.is_sparse() || src.is_sparse()) {
+    AT_ERROR("copy_() between dense and sparse Tensors is not implemented! Found self type = ",
+             self.type(), " and src type = ", src.type());
+  }
+
   if (self.is_same(src)) {
-    return;
+    return self;
   }
 
-  if (self.numel() == src.numel() && copy_transpose_valid(self, src)) {
-    return _copy_same_type_transpose_(self, src);
+  if (self.scalar_type() == kQUInt8) {
+    return quantized_copy_(self, src);
   }
 
-  copy_kernel_same_type(kCPU, self, src);
+  auto builder = TensorIterator::Builder();
+  builder.add_output(self);
+  builder.add_input(src);
+  builder.dont_resize_outputs();
+  builder.dont_compute_common_dtype();
+  auto iter = builder.build();
+
+  if (iter->numel() == 0) {
+    return self;
+  }
+
+  DeviceType device_type = iter->device_type(0);
+  if (iter->device_type(1) == kCUDA) {
+    device_type = kCUDA;
+  }
+
+  if (device_type == kCPU && copy_transpose_valid(self, src)) {
+    copy_same_type_transpose_(self, src);
+    return self;
+  }
+
+  copy_stub(device_type, *iter, non_blocking);
+  return self;
 }
 
-DEFINE_DISPATCH(copy_kernel_cast);
-DEFINE_DISPATCH(copy_kernel_same_type);
+DEFINE_DISPATCH(copy_stub);
 
 } // namespace native
 } // namespace at

aten/src/ATen/native/quantized/Copy.cpp
@@ -1,16 +1,18 @@
-#include <ATen/native/Copy.h>
+#include <ATen/native/quantized/Copy.h>
 
 #include <ATen/ATen.h>
-#include <ATen/native/TensorIterator.h>
-#include <ATen/native/cpu/Loops.h>
 #include <ATen/quantized/Quantizer.h>
 
 namespace at {
 namespace native {
-Tensor& _s_copy__quantized(Tensor& self, const Tensor& src, bool /* unused */) {
+Tensor& quantized_copy_(Tensor& self, const Tensor& src) {
   TORCH_CHECK(
       src.scalar_type() == at::kFloat,
       "Quantized copy only works with kFloat as source Tensor");
+  TORCH_CHECK(self.is_contiguous() && src.is_contiguous(),
+      "Quantized copy only works with contiguous Tensors");
+  TORCH_CHECK(self.sizes().equals(src.sizes()),
+      "Quantized copy only works with Tensors with the same shape");
   float* src_data = src.data<float>();
   AT_DISPATCH_QINT_TYPES(self.scalar_type(), "Copy", [&]() {
     scalar_t* self_data = self.data<scalar_t>();

aten/src/ATen/native/Copy.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include <ATen/ATen.h>
+#include <ATen/native/DispatchStub.h>
 
 namespace at {
+
+struct TensorIterator;
+
 namespace native {
 
 // Note [Implicit conversion between signed and unsigned]
@@ -42,6 +46,10 @@ struct inter_copy_type<uint8_t> {
 
 template <typename T>
 using inter_copy_type_t = typename inter_copy_type<T>::type;
+
+using copy_fn = void (*)(TensorIterator&, bool non_blocking);
+
+DECLARE_DISPATCH(copy_fn, copy_stub);
 
 } // namespace native
 } // namespace at

aten/src/ATen/native/TensorIterator.cpp
@@ -409,6 +409,23 @@ bool TensorIterator::is_trivial_1d() const {
   return ndim() == 1;
 }
 
+bool TensorIterator::is_contiguous() const {
+  if (numel() == 1) {
+    return true;
+  }
+  if (ndim() != 1) {
+    return false;
+  }
+  int num_tensors = ntensors();
+  for (int i = 0; i < num_tensors; i++) {
+    if (strides(i)[0] != element_size(i)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
 bool TensorIterator::is_scalar(int arg) const {
   const auto& stride = operands_[arg].stride_bytes;
   for (int i = 0; i < ndim(); i++) {

aten/src/ATen/native/TensorIterator.h
@@ -64,6 +64,7 @@ struct DimCounter {
   DimVector values;
   int64_t offset;
 };
+
 struct CAFFE2_API OperandInfo {
   OperandInfo() {}
   explicit OperandInfo(const Tensor& t, const Backend backend=Backend::Undefined, const ScalarType dtype=ScalarType::Undefined)
@@ -166,6 +167,8 @@ struct CAFFE2_API TensorIterator {
 
   /// 1-dimensional iteration and no buffering or type conversion
   bool is_trivial_1d() const;
+  /// Reducible to 1-dimensional and all operands are contiguous
+  bool is_contiguous() const;
   bool is_dim_reduced(int dim) const;
 
   /// Accessors for each operand
@@ -173,6 +176,7 @@ struct CAFFE2_API TensorIterator {
   void* data_ptr(int arg) const;
   ScalarType dtype(int arg=0) const { return operands_[arg].dtype; }
   DeviceType device_type(int arg=0) const { return backendToDeviceType(operands_[arg].backend); }
+  Device device(int arg=0) const { return operands_[arg].tensor.device(); }
   int64_t element_size(int arg) const { return elementSize(dtype(arg)); }
   bool is_scalar(int arg) const;
   bool is_cpu_scalar(int arg) const;

aten/src/ATen/native/cpu/CopyKernel.cpp
@@ -1,68 +1,53 @@
-#include <ATen/native/cpu/CopyKernel.h>
-
 #include <ATen/ATen.h>
-#include <ATen/CPUApplyUtils.h>
+
 #include <ATen/Dispatch.h>
-#include <ATen/cpu/vec256/vec256.h>
+#include <ATen/native/Copy.h>
 #include <ATen/native/TensorIterator.h>
 #include <ATen/native/cpu/Loops.h>
-#include <ATen/native/Copy.h>
 
 namespace at {
 namespace native {
 namespace {
 
 template <typename self_T>
-void copy_kernel_cast_t_impl(Tensor& self, const Tensor& src) {
-  auto builder = TensorIterator::Builder();
-  builder.add_output(self);
-  builder.add_input(src);
-  builder.dont_resize_outputs();
-  builder.dont_compute_common_dtype();
-  auto iter = builder.build();
-
+void copy_kernel_cast(TensorIterator& iter) {
   AT_DISPATCH_ALL_TYPES_AND2(
-      at::ScalarType::Half,
-      at::ScalarType::Bool,
-      src.scalar_type(),
+      ScalarType::Half,
+      ScalarType::Bool,
+      iter.dtype(1),
       "copy_kernel_cast",
       [&] {
-        at::native::unary_kernel(*iter, [=](scalar_t a) -> self_T {
+        at::native::unary_kernel(iter, [=](scalar_t a) -> self_T {
           return static_cast<self_T>(
               static_cast<at::native::inter_copy_type_t<self_T>>(a));
         });
       });
 }
 
-static void copy_kernel_cast_impl(Tensor& self, const Tensor& src) {
-  AT_DISPATCH_ALL_TYPES_AND2(at::ScalarType::Half, at::ScalarType::Bool,
-      self.scalar_type(), "copy_kernel_cast", [&]() { copy_kernel_cast_t_impl<scalar_t>(self, src); });
-}
-
-static void copy_kernel_same_type_impl(Tensor& self, const Tensor& src) {
-  auto builder = TensorIterator::Builder();
-  builder.add_output(self);
-  builder.add_input(src);
-  builder.dont_resize_outputs();
-  auto iter = builder.build();
-
-  if (self.scalar_type() == at::ScalarType::Half) {
-    unary_kernel(*iter, [=](at::Half a) -> at::Half { return a; });
+static void copy_kernel(TensorIterator& iter, bool non_blocking) {
+  ScalarType dtype = iter.dtype(0);
+  if (dtype == iter.dtype(1)) {
+    if (dtype == ScalarType::Half) {
+      unary_kernel(iter, [=](at::Half a) -> at::Half { return a; });
+    } else {
+      AT_DISPATCH_ALL_TYPES_AND(
+          ScalarType::Bool, dtype, "copy_kernel", [&] {
+            unary_kernel_vec(
+                iter,
+                [=](scalar_t a) -> scalar_t { return a; },
+                [=](Vec256<scalar_t> a) { return a; });
+          });
+    }
   } else {
-    AT_DISPATCH_ALL_TYPES_AND(
-        at::ScalarType::Bool, self.scalar_type(), "copy_kernel_same_type", [&] {
-          unary_kernel_vec(
-              *iter,
-              [=](scalar_t a) -> scalar_t { return a; },
-              [=](Vec256<scalar_t> a) { return a; });
-        });
+    AT_DISPATCH_ALL_TYPES_AND2(ScalarType::Half, ScalarType::Bool, dtype, "copy_", [&] {
+      copy_kernel_cast<scalar_t>(iter);
+    });
   }
 }
 
 } // anonymous namespace
 
-REGISTER_DISPATCH(copy_kernel_same_type, &copy_kernel_same_type_impl);
-REGISTER_DISPATCH(copy_kernel_cast, &copy_kernel_cast_impl);
+REGISTER_DISPATCH(copy_stub, &copy_kernel);
 
 } // namespace native
 } // namespace at

aten/src/ATen/native/cpu/CopyKernel.h
@@ -1,15 +1,0 @@
-#pragma once
-
-#include <ATen/ATen.h>
-#include <ATen/native/DispatchStub.h>
-
-namespace at {
-namespace native {
-
-using forward_fn = void (*)(Tensor&, const Tensor&);
-
-DECLARE_DISPATCH(forward_fn, copy_kernel_same_type);
-DECLARE_DISPATCH(forward_fn, copy_kernel_cast);
-
-} // namespace native
-} // namespace at

commit 2fc31acbeee08d49ba0861881d3ffb04346f61d2

Fix copy_transpose_valid check (#20759)
Summary:
Fixes #20755

(Was broken in #20685)

cc vadimkantorov
Pull Request resolved: https://github.com/pytorch/pytorch/pull/20759

Differential Revision: D15433712

Pulled By: colesbury

fbshipit-source-id: 29f612f7d4d7b73158d6f5dc1e46fd2f8fb09a2f

aten/src/ATen/native/Copy.cpp
@@ -14,6 +14,7 @@ bool copy_transpose_valid(const Tensor& self, const Tensor& src) {
   const int MIN_SZ = 60 * 60;
   return self.is_contiguous() && src.numel() != 0 && src.dim() == 2 &&
       src.stride(0) == 1 && src.stride(1) == src.size(0) &&
+      self.scalar_type() == src.scalar_type() &&
       self.numel() >= MIN_SZ;
 }
 

test/test_torch.py
@@ -2644,6 +2644,18 @@ class _TestTorchMixin(object):
             copied_dtype = copy.deepcopy(dtype)
             self.assertIs(dtype, copied_dtype)
 
+    def test_copy_transpose(self):
+        x = torch.arange(100 * 100, dtype=torch.float).reshape(100, 100).t()
+        y = torch.empty(100, 100, dtype=torch.float)
+        y.copy_(x)
+        self.assertEqual(y[:, 0], range(100))
+        self.assertEqual(y[:, 40], range(4000, 4100))
+
+        y = torch.empty(100, 100, dtype=torch.double)
+        y.copy_(x)
+        self.assertEqual(y[:, 0], range(100))
+        self.assertEqual(y[:, 40], range(4000, 4100))
+
     def test_device(self):
         cpu = torch.device('cpu')
         self.assertEqual('cpu', str(cpu))