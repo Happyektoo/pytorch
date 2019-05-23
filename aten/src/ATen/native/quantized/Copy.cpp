Happyektoo/pytorch

commit 272a510119a0379c11860c69a1683dc4f239f43b

Add QInt32 ScalarType and qint32 data type (#19816)
Summary:
Pull Request resolved: https://github.com/pytorch/pytorch/pull/19816

We need this for quantization for bias
add third argument of ScalarType to `quantize_linear`

Differential Revision: D15094174

fbshipit-source-id: f19ec8f4716cf5fe0aa21b38d45af6d27c9ab377

aten/src/ATen/native/quantized/Copy.cpp
@@ -14,14 +14,16 @@ Tensor& _s_copy__quantized(Tensor& self, const Tensor& src, bool /* unused */) {
   TORCH_CHECK(
       src.scalar_type() == at::kFloat,
       "Quantized copy only works with kFloat as source Tensor");
-  qint8* self_data = self.data<qint8>();
   float* src_data = src.data<float>();
-  for (int i = 0; i < self.numel(); ++i) {
-    self_data[i] = quantize_uint8(
-        self.q_scale().to<float>(),
-        self.q_zero_point().to<uint8_t>(),
-        src_data[i]);
-  }
+  AT_DISPATCH_QINT_TYPES(self.scalar_type(), "Copy", [&]() {
+    scalar_t* self_data = self.data<scalar_t>();
+    for (int i = 0; i < self.numel(); ++i) {
+      self_data[i] = quantize_val<scalar_t>(
+          self.q_scale().to<float>(),
+          self.q_zero_point().to<int32_t>(),
+          src_data[i]);
+    }
+  });
   return self;
 }
 } // namespace native

aten/src/ATen/DLConvertor.cpp
@@ -42,6 +42,9 @@ static DLDataType getDLDataType(const Tensor& t) {
     case ScalarType::QInt8:
       throw std::logic_error("QInt8 is not supported by dlpack");
       break;
+    case ScalarType::QInt32:
+      throw std::logic_error("QInt32 is not supported by dlpack");
+      break;
     case ScalarType::ComplexHalf:
       throw std::logic_error("ComplexHalf is not supported by dlpack");
     case ScalarType::ComplexFloat:

aten/src/ATen/Dispatch.h
@@ -208,6 +208,18 @@ inline void deprecated_AT_DISPATCH_ALL_TYPES_AND_HALF_AND_COMPLEX() {}
     }                                                                        \
   }()
 
+#define AT_DISPATCH_QINT_TYPES(TYPE, NAME, ...)                         \
+  [&] {                                                                 \
+    switch (TYPE) {                                                     \
+      AT_PRIVATE_CASE_TYPE(                                             \
+          at::ScalarType::QInt8, qint8, __VA_ARGS__)                    \
+      AT_PRIVATE_CASE_TYPE(                                             \
+          at::ScalarType::QInt32, qint32, __VA_ARGS__)                  \
+      default:                                                          \
+        AT_ERROR(#NAME, " not implemented for '", toString(TYPE), "'");  \
+    }                                                                   \
+  }()
+
 #define AT_DISPATCH_ALL_TYPES_AND_COMPLEX(TYPE, NAME, ...)                   \
   [&] {                                                                      \
     const auto& the_type = TYPE;                                             \

aten/src/ATen/core/Tensor.h
@@ -580,7 +580,7 @@ class CAFFE2_API Tensor {
   Tensor to_sparse(int64_t sparse_dim) const;
   Tensor to_sparse() const;
   Tensor to_mkldnn() const;
-  Tensor quantize_linear(double scale, int64_t zero_point) const;
+  Tensor quantize_linear(double scale, int64_t zero_point, ScalarType dtype) const;
   Tensor dequantize() const;
   Scalar q_scale() const;
   Scalar q_zero_point() const;

aten/src/ATen/core/TensorMethods.h
@@ -797,8 +797,8 @@ inline Tensor Tensor::to_sparse() const {
 inline Tensor Tensor::to_mkldnn() const {
     return dispatch_type().to_mkldnn(*this);
 }
-inline Tensor Tensor::quantize_linear(double scale, int64_t zero_point) const {
-    return dispatch_type().quantize_linear(*this, scale, zero_point);
+inline Tensor Tensor::quantize_linear(double scale, int64_t zero_point, ScalarType dtype) const {
+    return dispatch_type().quantize_linear(*this, scale, zero_point, dtype);
 }
 inline Tensor Tensor::dequantize() const {
     return dispatch_type().dequantize(*this);

aten/src/ATen/core/Type.h
@@ -390,7 +390,7 @@ struct CAFFE2_API Type {
   virtual Tensor to_sparse(const Tensor & self, int64_t sparse_dim) const = 0;
   virtual Tensor to_sparse(const Tensor & self) const = 0;
   virtual Tensor to_mkldnn(const Tensor & self) const = 0;
-  virtual Tensor quantize_linear(const Tensor & self, double scale, int64_t zero_point) const = 0;
+  virtual Tensor quantize_linear(const Tensor & self, double scale, int64_t zero_point, ScalarType dtype) const = 0;
   virtual Tensor dequantize(const Tensor & self) const = 0;
   virtual Scalar q_scale(const Tensor & self) const = 0;
   virtual Scalar q_zero_point(const Tensor & self) const = 0;

aten/src/ATen/gen.py
@@ -189,6 +189,7 @@ extension_backends = ['MSNPU', 'XLA']
 # scalar_name, c_type, accreal, is_floating_type
 quantized_scalar_types = [
     ('QInt8', 'qint8', 'QInt8AccrealNotDefined', 'Qint8IsFloatingTypeNotDefined'),
+    ('QInt32', 'qint32', 'QInt32AccrealNotDefined', 'Qint32IsFloatingTypeNotDefined'),
 ]
 
 

aten/src/ATen/native/native_functions.yaml
@@ -2564,7 +2564,7 @@
 
 - func: to_mkldnn_backward(Tensor grad, Tensor input) -> Tensor
 
-- func: quantize_linear(Tensor self, float scale, int zero_point) -> Tensor
+- func: quantize_linear(Tensor self, float scale, int zero_point, ScalarType dtype) -> Tensor
   variants: function, method
   dispatch:
     CPU: quantize_linear_cpu

commit 0b63d4bf12c340cd942890cf8b9cbfb4c7642ad5

Fix build error with MSVC (#20853)
Summary:
Close #20642

Possibly broken by #19816
Pull Request resolved: https://github.com/pytorch/pytorch/pull/20853

Differential Revision: D15474620

Pulled By: jerryzh168

fbshipit-source-id: 99b52d92a93bac7cab52537f1ebdbd286d4b2cfe

aten/src/ATen/native/quantized/Copy.cpp
@@ -13,8 +13,8 @@ Tensor& quantized_copy_(Tensor& self, const Tensor& src) {
       "Quantized copy only works with contiguous Tensors");
   TORCH_CHECK(self.sizes().equals(src.sizes()),
       "Quantized copy only works with Tensors with the same shape");
-  float* src_data = src.data<float>();
   AT_DISPATCH_QINT_TYPES(self.scalar_type(), "Copy", [&]() {
+    float* src_data = src.data<float>();
     scalar_t* self_data = self.data<scalar_t>();
     for (int i = 0; i < self.numel(); ++i) {
       self_data[i] = quantize_val<scalar_t>(