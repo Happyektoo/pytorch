Happyektoo/pytorch

commit 9ad51771e777db74de06c1ac615d476f63dc3d3b

pin_memory thread now uses 1 thread only (#25111)
Summary:
Fixes https://github.com/pytorch/pytorch/issues/25010
Pull Request resolved: https://github.com/pytorch/pytorch/pull/25111

Differential Revision: D16992718

Pulled By: soumith

fbshipit-source-id: fe23721d4cc293fa245c84c656241730335077dd

torch/utils/data/_utils/pin_memory.py
@@ -12,6 +12,10 @@ from torch._utils import ExceptionWrapper
 
 
 def _pin_memory_loop(in_queue, out_queue, device_id, done_event):
+    # This setting is thread local, and prevents the copy in pin_memory from
+    # consuming all CPU cores.
+    torch.set_num_threads(1)
+    
     torch.cuda.set_device(device_id)
 
     # See NOTE [ Data Loader Multiprocessing Shutdown Logic ] for details on the

commit 06dd62d0f66a924bddc3c939fcbb73f6c828a3ba

Fix lint checker breakage caused by #25111 (#25122)
Summary:
fix lint by flake8
Pull Request resolved: https://github.com/pytorch/pytorch/pull/25122

Differential Revision: D16995103

Pulled By: zrphercule

fbshipit-source-id: 810be4d8073cae73d4b0f6d82b410fd235a73bbb

torch/utils/data/_utils/pin_memory.py
@@ -15,7 +15,7 @@ def _pin_memory_loop(in_queue, out_queue, device_id, done_event):
     # This setting is thread local, and prevents the copy in pin_memory from
     # consuming all CPU cores.
     torch.set_num_threads(1)
-    
+
     torch.cuda.set_device(device_id)
 
     # See NOTE [ Data Loader Multiprocessing Shutdown Logic ] for details on the